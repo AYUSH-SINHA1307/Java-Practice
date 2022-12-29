7b201a777ad0d1235da4c031787e0cb835b9fccf
Ayush Sinha <114801569+AYUSH-SINHA1307@users.noreply.github.com>
2022-12-29 19:38:08 +0530

    Add files via upload

---
+++ java/firstjava.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class firstjava {
+    public static void main(String args[]) {
+       int number = 2;
+       switch(number) {
+       
+
+     }
+}
---
+++ java/forloop.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class forloop {
+
+    public static void main(String args[]) {
+        //int i = 1;
+        for(int i=1; i<=10; i++) {
+            System.out.println("Hello world");
+            
+        }
+
+    }
+
+}
